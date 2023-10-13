456739f0d8b7b0d8d51c285872b4f8d164682da1
Rhiannon Rognstad <rlrognstad@gmail.com>
2023-10-13 16:53:01 -0400

    Updates to repo-research-pilot

---
+++ src/data/s3_download_sso.py
@@ -0,0 +1,3 @@
+# copies training data to specified location from s3 if using SSO autentication 
+# see SSO configuration setup at:
+# https://docs.aws.amazon.com/cli/latest/userguide/sso-configure-profile-token.html#sso-configure-profile-token-auto-sso
