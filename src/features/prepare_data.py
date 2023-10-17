9cc331b078a1180ad558d61ebbb79765a28a1317
Rhiannon Rognstad <rlrognstad@gmail.com>
2023-10-17 10:59:42 -0400

    refactored imagery loading functions

--- src/features/prepare_data.py
+++ src/features/prepare_data.py
@@ -6,6 +6,8 @@ import yaml
 from tqdm import tqdm
 from utils.logs import get_logger
 import data.clean_ceo_summary as cc
+import features.slow_glcm as slow_txt
+import features.fast_glcm as fast_txt
 
 
 def get_ceo_plot_ids(v_train_data, config_path, label_type=['binary']):
@@ -64,96 +66,184 @@ def get_ceo_plot_ids(v_train_data, config_path, label_type=['binary']):
             plot_ids.remove(plot)
     return plot_ids
 
-def binary_ceo(v_train_data, config_path):
-    '''
-    Creates a list of plot ids to process from collect earth surveys 
-    with binary class labels (0, 1). Drops all plots w/o s2 imagery. 
-    Returns list of plot_ids.
-    '''
-    with open(config_path) as conf_file:
-        config = yaml.safe_load(conf_file)
-    # use CEO csv to gather plot id numbers
-    plot_ids = []
 
-    for i in v_train_data:
-        ceo_path = config['data_load']['ceo_survey_directory']
-        df = pd.read_csv(f'{ceo_path}/ceo-plantations-train-{i}.csv')
+def load_slope(idx, local_data_path):
+    """
+    Slope is stored as a 32 x 32 float32 array with border information.
+    Needs to be converted to 1 x 14 x 14 array to match the labels w/ new axis.
+    directory = '../data/train-slope/'
+    """
+    directory = local_data_path + 'train-slope/'
+    # Remove axes of length one with .squeeze (32 x 32 x 1)
+    x = np.load(directory + str(idx) + '.npy').squeeze()
+
+    # slice out border information
+    border_x = (x.shape[0] - 14) // 2
+    border_y = (x.shape[1] - 14) // 2
+    slope = x[border_x:-border_x, border_y:-border_y]
+
+    # explicitly convert array to a row vector, adding axis (1 x 14 x 14)
+    slope = slope[np.newaxis]
+    
+    return slope
+
+def load_s1(idx, local_data_path):
+    """
+    S1 is stored as a (12, 32, 32, 2) float64 array with border information.
+    Needs to be converted from monthly mosaics to an annual median, 
+    and remove border information to match labels. 
+    Dtype needs to be converted to float32.
+    """
+
+    # since s1 is a float64, can't use to_float32() 
+    directory = local_data_path + 'train-s1/'
+    s1 = hkl.load(directory + str(idx) + '.hkl')
+    
+    # convert to decible
+    s1[..., -1] = convert_to_db(s1[..., -1], 22)
+    s1[..., -2] = convert_to_db(s1[..., -2], 22)
+
+    # get the median across flattened array
+    if len(s1.shape) == 4:
+        s1 = np.median(s1, axis = 0, overwrite_input=True)
+
+    # just convert (removed /65535) keep here
+    s1 = np.float32(s1)
+
+    # slice out border information (32, 32, 2) -> (14, 14, 2)
+    border_x = (s1.shape[0] - 14) // 2
+    border_y = (s1.shape[1] - 14) // 2
+    s1 = s1[border_x:-border_x, border_y:-border_y]
+
+    return s1
+
+def load_s2(idx, local_data_path):
+    
+    """
+    S2 is stored as a (12, 28, 28, 11) uint16 array. 
+    Remove the last axis index - the date of the imagery. 
+    Convert monthly images to an annual median. 
+    Remove the border to correspond to the labels.
+    Convert to float32.
+    """
+    directory = local_data_path + 'train-s2/'
+    s2 = hkl.load(directory + str(idx) + '.hkl')
+
+    # remove date of imagery (last axis)
+    if s2.shape[-1] == 11:
+        s2 = np.delete(s2, -1, -1)
+
+    # checks for floating datatype, if not converts to float32
+    # TODO check if this is the same func as in deply pipeline
+    # if so consider moving to utils
+    if not isinstance(s2.flat[0], np.floating):
+        assert np.max(s2) > 1
+        s2 = s2.astype(np.float32) / 65535
+        assert np.max(s2) < 1
+ 
+    # convert monthly images to annual median
+    if len(s2.shape) == 4:
+        s2 = np.median(s2, axis = 0, overwrite_input=True)
+
+    # slice out border information
+    border_x = (s2.shape[0] - 14) // 2
+    border_y = (s2.shape[1] - 14) // 2
+    s2 = s2[border_x:-border_x, border_y:-border_y].astype(np.float32)
+
+    return s2
+
+def load_ttc(idx, local_data_path):
+    '''
+    Features are stored as a 14 x 14 x 65 float64 array. The last axis contains 
+    the feature dimensions. Dtype needs to be converted to float32. The TML
+    probability/prediction can optionally be dropped.
+
+    ## Update per 2/13/23
+    Features range from -infinity to +infinity
+    and must be clipped to be consistent with the deployed features.
+    
+    Index 0 ([...,0]) is the tree cover prediction from the full TML model
+    Index 1 - 33 are high level features
+    Index 33 - 65 are low level features
+    '''
+    directory = local_data_path + 'train-features-ckpt-2023-02-09/'
+    feats = hkl.load(directory + str(idx) + '.hkl')
+
+    # clip all features after indx 0 to specific vals
+    feats[..., 1:] = np.clip(feats[..., 1:], a_min=-32.768, a_max=32.767)
+
+    feats = feats.astype(np.float32)
+
+    return feats
+
+def load_label(idx, classes, local_data_path):
+    '''
+    The labels are stored as a binary 14 x 14 float64 array.
+    Unless they are stored as (196,) and need to be reshaped.
+    Dtype needs to be converted to float32.
+    '''
+    directory = local_data_path + 'train-labels/'
+    labels_raw = np.load(directory + str(idx) + '.npy')
+
+    if len(labels_raw.shape) == 1:
+        labels_raw = labels_raw.reshape(14, 14)
+
+    # makes sure that a binary classification exercise updates
+    # any multiclass labels (this is just converting AF label (2) to 1)
+    if classes == 'binary':
+        labels = labels_raw.copy()
+        labels[labels_raw == 2] = 1
+        labels = labels.astype(np.float32)
         
-        # for multiclass surveys, change labels
-        multiclass = ['v08', 'v14', 'v15']
-        if i in multiclass:
-        
-            # map label categories to ints
-            # this step might not be needed but leaving for now.
-            df['PLANTATION_MULTI'] = df['PLANTATION'].map({'Monoculture': 1,
-                                                        'Agroforestry': 2,
-                                                        'Not plantation': 0,
-                                                        'Unknown': 255})
+    else:
+        labels = labels_raw.astype(np.float32)
+    
+    return labels
 
-            # confirm that unknown labels are always a full 14x14 (196 points) of unknowns
-            # if assertion fails, will print count of points
-            unknowns = df[df.PLANTATION_MULTI == 255]
-            for plot in set(list(unknowns.PLOT_ID)):
-                assert len(unknowns[unknowns.PLOT_ID == plot]) == 196, f'{plot} has {len(unknowns[unknowns.PLOT_ID == plot])}/196 points labeled unknown.'
-
-            # drop unknown samples
-            df_new = df.drop(df[df.PLANTATION_MULTI == 255].index)
-            print(f'{(len(df) - len(df_new)) / 196} plots labeled unknown were dropped from {i}.')
-            
-            plot_ids = plot_ids + df_new.PLOT_FNAME.drop_duplicates().tolist()
-
-        # for binary surveys add to list
-        else:
-            plot_ids = plot_ids + df.PLOT_FNAME.drop_duplicates().tolist()
-
-    # if the plot_ids do not have 5 digits, change to str and add leading 0
-    plot_ids = [str(item).zfill(5) if len(str(item)) < 5 else str(item) for item in plot_ids]
-
-    # check and remove any plot ids where there are no cloud free images (no s2 hkl file)
-    print('warning needs to be updated')
-    for plot in plot_ids[:]:   
-        local_path= config['data_load']['local_prefix']         
-        if not os.path.exists(f'{local_path}/train-s2/{plot}.hkl'.strip()):
-            print(f'Plot id {plot} has no cloud free imagery and will be removed.')
-            plot_ids.remove(plot)
-    return plot_ids
-
-def multiclass_ceo(v_train_data, config_path):
+def load_txt(idx, local_Data_path):
+    
     '''
-    Creates a list of plot ids to process from collect earth surveys 
-    with multi-class labels (0, 1, 2, 255). Drops all plots with 
-    "unknown" labels and plots w/o s2 imagery. Returns list of plot_ids.
+    Loads raw s2 data and preprocesses
+    in order to extract texture features for RGB and NIR 
+    bands. Outputs the texture analysis as a (14, 14, 16) 
+    array as a float32 array.
     '''
-    with open(config_path) as conf_file:
-        config = yaml.safe_load(conf_file)
-    # use CEO csv to gather plot id numbers
-    plot_ids = []
-
-    for i in v_train_data:
-        ceo_path = config['data_load']['ceo_survey_directory']
-        # for each training data survey, drop all unknown labels
-        df = pd.read_csv(f'{ceo_path}/ceo-plantations-train-{i}.csv')
-
-        # assert unknown labels are always a full 14x14 (196 points) of unknowns
-        unknowns = df[df.PLANTATION == 255]
-        for plot in set(list(unknowns.PLOT_ID)):
-            assert len(unknowns[unknowns.PLOT_ID == plot]) == 196,\
-            f'{plot} has {len(unknowns[unknowns.PLOT_ID == plot])}/196 points labeled unknown.'
-
-        # drop unknowns and add to full list
-        df_new = df.drop(df[df.PLANTATION == 255].index)
-        print(f'{int((len(df) - len(df_new)) / 196)} plots labeled unknown were dropped from {i}.')
-        plot_ids += df_new.PLOT_FNAME.drop_duplicates().tolist()
-
-    # add leading 0 to plot_ids that do not have 5 digits
-    plot_ids = [str(item).zfill(5) if len(str(item)) < 5 else str(item) for item in plot_ids]
+    directory = local_data_path + 'train-s2/'
+    
+    if os.path.exists(f'{local_data_path}/train-texture/{idx}.npy'):
+        output = np.load(f'{local_data_path}/train-texture/{idx}.npy')
+    
+    else: 
+        #print('Calculating GLCM texture features...')
+        s2 = hkl.load(directory + str(idx) + '.hkl')
         
-    # remove any plot ids where there are no cloud free images (no s2 hkl file)
-    local_path= config['data_load']['local_prefix']
-    final_plots = [plot for plot in plot_ids if os.path.exists(f'{local_path}/train-s2/{plot}.hkl')]
-    print(f'{len(plot_ids) - len(final_plots)} plots had no cloud free imagery and will be removed.')
+        # remove date of imagery (last axis)
+        if s2.shape[-1] == 11:
+            s2 = np.delete(s2, -1, -1)
 
-    return final_plots
+        # convert monthly images to annual median
+        if len(s2.shape) == 4:
+            s2 = np.median(s2, axis = 0, overwrite_input=True)
+
+        # this has to be done after median
+        # doesnt work if just calling .astype(np.uint8)
+        s2 = ((s2.astype(np.float32) / 65535) * 255).astype(np.uint8)
+        
+        blue = s2[..., 0]
+        green = s2[..., 1]
+        red = s2[..., 2]
+        nir = s2[..., 3]
+        output = np.zeros((14, 14, 16))
+        
+        output[..., 0:4] = slow_txt.extract_texture(blue)
+        output[..., 4:8] = slow_txt.extract_texture(green)
+        output[..., 8:12] = slow_txt.extract_texture(red)
+        output[..., 12:16] = slow_txt.extract_texture(nir)
+
+        # save the output
+        np.save(f'../data/train-texture/{idx}.npy', output)
+    
+    return output.astype(np.float32)
 
 def create_xy(v_train_data, classes, drop_feats, config_path, feature_select=[], verbose=False):
     '''
@@ -171,12 +261,11 @@ def create_xy(v_train_data, classes, drop_feats, config_path, feature_select=[],
     convert_binary:
     
     '''
-    
+    with open(config_path) as conf_file:
+        config = yaml.safe_load(conf_file)
     # need to be able to create xy for 1) binary only 2) multiclass only 3) binary and multi
-    plot_ids = get_ceo_plot_ids(v_train_data, config_path, classes)
-    
+    plot_ids = get_ceo_plot_ids(v_train_data, config_path, classes)    
     print(f'Training data includes {len(plot_ids)} plots.')
-
 
     # create empty x and y array based on number of plots (dropping TML probability changes dimensions from 78 -> 77)
     sample_shape = (14, 14)
@@ -190,23 +279,24 @@ def create_xy(v_train_data, classes, drop_feats, config_path, feature_select=[],
     else:
         x_all = np.zeros(shape=(n_samples, 14, 14, 94))
 
+    local_data_path = config['data_load']['local_prefix']
     for num, plot in enumerate(tqdm(plot_ids)):
 
         if drop_feats:
-            slope = load_slope(plot)
-            s1 = load_s1(plot)
-            s2 = load_s2(plot)
+            slope = load_slope(plot, local_data_path)
+            s1 = load_s1(plot, local_data_path)
+            s2 = load_s2(plot, local_data_path)
             X = make_sample_nofeats(sample_shape, slope, s1, s2)
             y = load_label(plot, classes)
             x_all[num] = X
             y_all[num] = y
 
         else:
-            slope = load_slope(plot)
-            s1 = load_s1(plot)
-            s2 = load_s2(plot)
-            ttc = load_ttc(plot)
-            txt = load_txt(plot)
+            slope = load_slope(plot,local_data_path)
+            s1 = load_s1(plot, local_data_path)
+            s2 = load_s2(plot, local_data_path)
+            ttc = load_ttc(plot, local_data_path)
+            txt = load_txt(plot, local_data_path)
             #print('REMINDER: Import fast or slow txt feats?')
             #feats = load_feats(plot, import_txt=False)
             validate.train_output_range_dtype(slope, s1, s2, ttc, feature_select)
