655076c2a0aba8881b96cf966ce98cd1f9e4915b
Rhiannon Rognstad <rlrognstad@gmail.com>
2023-10-18 10:20:43 -0400

    updated stage_featureize

---
+++ src/stage_featureize.py
@@ -0,0 +1,41 @@
+import argparse
+import joblib
+from typing import Text
+import yaml
+import json
+import pickle
+from utils.logs import get_logger
+import features.prepare_data as prep
+
+def featureize(config_path: Text) -> None:
+    with open(config_path) as conf_file:
+        config = yaml.safe_load(conf_file)
+    logger = get_logger('FEATURIZE', log_level=config['base']['log_level'])
+    with open(config['data_load']['ceo_json'], 'r') as fp:
+        ceo_batch_list = json.load(fp)
+        logger.info('CEO survey targets loaded')
+    X, y = prep.create_xy(ceo_batch_list, 
+                    classes='multi', 
+                    drop_feats=False,
+                    config_path =config_path, 
+                    verbose=False)
+    logger.info('X,y features loaded')
+    X_train, X_test, y_train, y_test = prep.reshape_training_data(X, y, config_path, config['data_condition']['scale_features'])
+    scale_data = config['data_condition']['scale_features']
+    logger.info(f'Train and test set generated, scaled data: {scale_data}')
+    with open(config['data_condition']['train_data_x'], 'wb') as fp:
+        pickle.dump(X_train, fp)
+    with open(config['data_condition']['train_data_y'], 'wb') as fp:
+        pickle.dump(y_train, fp)
+    with open(config['data_condition']['test_data_x'], 'wb') as fp:
+        pickle.dump(X_test, fp)
+    with open(config['data_condition']['test_data_y'], 'wb') as fp:
+        pickle.dump(y_test, fp)
+    logger.info("Training and testing data exported")
+    
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+    featureize(config_path=args.config)
+    
