9d617d27e198e48526ccbe22896a2b71b2be03c7
Jessica Ertel <jessertel@mac.com>
2024-01-04 14:14:08 -0500

    test fs implementation

--- src/model/feature_selection.py
+++ src/model/feature_selection.py
@@ -16,7 +16,7 @@ import shap
 import model.train as trn
 from utils.logs import get_logger
 
-def OLD_feature_selection(model, feat_count):
+def OLD_feature_importance(model, feat_count):
     
     '''
     Calculates the feature importance score for a given model
@@ -57,16 +57,18 @@ def least_imp_feature(model, X_test, logger):
     '''
     # create shap explainer
     explainer = shap.Explainer(model)
-    logger.debug("SHAP explainer calculated")
+    # logger.debug("SHAP explainer calculated")
     shap_values = explainer(X_test)
-    logger.debug("SHAP values calculated")
-    logger.debug(shap_values.shape)
+    # logger.debug("SHAP values calculated")
+    # shap value shape will be (28420, num_feats, 4)
+    # logger.debug(f'Shap values shape: {shap_values.shape}') 
+
      # Calculate the mean absolute SHAP values across instances and features
     feature_importance = shap_values.abs.mean(0).mean(1).values
-    logger.debug(X_test.columns)
-    importance_df = pd.DataFrame(
-        {"features": X_test.columns, "importance": feature_importance}
-    )
+    logger.debug(f'columns:{X_test.columns}')
+    importance_df = pd.DataFrame({"features": X_test.columns, 
+                                  "importance": feature_importance}
+                                  )
     importance_df.sort_values(by="importance", ascending=False, inplace=True)
 
     return importance_df["features"].iloc[-1]
@@ -87,12 +89,15 @@ def backward_selection(X_train,
     to incrementally remove features from the training set until the metric 
     no longer improves. This function returns the dataframe with the features 
     that give the best metric. Return at most max_features.
+
+    Requires scaled, full set of 94 features in X_train, X_test
+    Metric being used here is balanced accuracy
     """
     # get baseline metric
     total_features = X_train.shape[1]
     select_X_train = pd.DataFrame(X_train.copy())
     select_X_test = pd.DataFrame(X_test.copy())
-    metric, model, X_test = trn.train(
+    metric, model, X_test = trn.fit_estimator(
         X_train,
         X_test,
         y_train,
@@ -103,7 +108,7 @@ def backward_selection(X_train,
         fit_params_dict,
     )
     logger.debug(f"X_test shape: {X_test.shape}")
-    logger.info(f"{metric} with {select_X_train.shape[1]} features")
+    logger.info(f"BASELINE: {round(metric, 4)} with {select_X_train.shape[1]} features")
     last_metric = metric
 
     # Drop least important feature and recalculate model peformance
@@ -118,8 +123,8 @@ def backward_selection(X_train,
         tmp_X_train = select_X_train.drop(columns=[dropped_feature])
         tmp_X_test = select_X_test.drop(columns=[dropped_feature])
 
-        # Rerun modeling
-        metric, fs_model, X_test = trn.fit_estimator(
+        # Rerun modeling - depending on what format select x train is, this could be a separate step
+        metric, model, X_test = trn.fit_estimator(
             tmp_X_train,
             tmp_X_test,
             y_train,
@@ -129,14 +134,16 @@ def backward_selection(X_train,
             model_params_dict,
             fit_params_dict,
         )
-        logger.info(f"{metric} with {tmp_X_train.shape[1]} features")
+        logger.info(f"{round(metric, 4)} with {tmp_X_train.shape[1]} features")
+        # not sure this is working?
         if (num_features < max_features) and (metric < last_metric):
             # metric decreased, return last dataframe
-            return select_X_train, select_X_test
+            return select_X_train, select_X_test, model
         else:
             # metric improved, continue dropping features
             last_metric = metric
             select_X_train = tmp_X_train
             select_X_test = tmp_X_test
 
-    return select_X_train, select_X_test, fs_model
+    # confirm which model is being returned here
+    return select_X_train, select_X_test, model
