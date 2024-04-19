21e68fe48c4a5fcc79628bf0ee2afe277d5512b0
Jessica Ertel <jessertel@mac.com>
2024-04-19 16:10:22 -0400

    fix upload filepath and shapefile exception

--- src/utils/mosaic.py
+++ src/utils/mosaic.py
@@ -11,7 +11,49 @@ import sys
 from datetime import datetime
 import boto3
 import hickle as hkl
-import copy
+import boto3
+import botocore
+
+def download_shape(data_dir: str, 
+                   location: list, 
+                   bucket,
+                   aws_access_key: str, 
+                   aws_secret_key: str):
+    '''
+    Checks to see if the location shapefile exists locally,
+    if not downloads the file from s3
+    '''
+
+    s3_dir = f'2020/shapefiles/{location[1]}/'
+    dest_dir = f"{data_dir}shapefiles/"
+    s3 = boto3.resource('s3',
+                aws_access_key_id=aws_access_key, 
+                aws_secret_access_key=aws_secret_key)
+    bucket = s3.Bucket(bucket)
+    
+    # skip if shapefile present
+    if os.path.exists(f"{data_dir}shapefiles/{location[1]}.shp"):
+        print(f'Shapefile for {location[1]} exists locally.')
+
+    else:
+        for obj in bucket.objects.filter(Prefix=s3_dir):
+            target = os.path.join(dest_dir, os.path.relpath(obj.key, s3_dir))
+            if not os.path.exists(os.path.dirname(target)):
+                os.makedirs(os.path.dirname(target))
+            if obj.key[-1] == '/':
+                continue
+            try:
+                bucket.download_file(obj.key, target)
+
+            # if the tiles do not exist on s3, catch the error and return False
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "404":
+                    print("Shapefile is not on s3.")
+                    return False
+                
+        print(f"Shapefile downloaded for {location[1]}.")
+
+    return True
 
 def mosaic_tif(location: list, version: str, tiles: list):
 
@@ -65,17 +107,25 @@ def mosaic_tif(location: list, version: str, tiles: list):
 
     return None
 
-def clip_it(location: list, version: str, shapefile: str):
+def clip_it(aak: str, 
+            ask: str, 
+            location: list, 
+            version: str,
+            dir:str,
+            bucket: str):
     ''''
     imports a mosaic tif and clips it to the extent of a 
     given shapefile
     '''
+    successful = download_shape(dir, location, bucket, aak, ask)
+    
     mosaic_dir = f'tmp/{location[0]}/preds/mosaic/'
     date = datetime.today().strftime('%Y-%m-%d')
     merged = f'{mosaic_dir}{location[1]}_{version}_{date}_mrgd.tif'
     clipped = f'{mosaic_dir}{location[1]}_{version}_{date}.tif'
     
-    if os.path.exists(shapefile):
+    if successful:
+        shapefile = f"data/shapefiles/{location[1]}.shp"
         shapefile = gpd.read_file(shapefile)
         with rs.open(merged) as src:
             shapefile = shapefile.to_crs(src.crs)
@@ -95,44 +145,43 @@ def clip_it(location: list, version: str, shapefile: str):
         with rs.open(clipped,'w',**out_meta) as dst:
             dst.write(out_image)
         os.remove(merged)
+        return clipped
+    
     else:
         print("Shapefile does not exist - skipping clip.")
-    
-    return None
+        return merged
 
 
-def upload_mosaic(location: list, aws_access_key: str, aws_secret_key: str):
+def upload_mosaic(aak: str, 
+                  ask: str, 
+                  filename: str):
     '''
     Uploads the combined tif to an s3 bucket
     '''
-    date = datetime.today().strftime('%Y-%m-%d')
-    
-    # outpath will be the new filename
-    suffix = f'{location[0]}_{version}_{date}.tif'
-    mosaic_filepath = f'tmp/{location[0]}/preds/mosaic/{suffix}'
-
     s3 = boto3.resource('s3',
-                        aws_access_key_id=aws_access_key, 
-                        aws_secret_access_key=aws_secret_key)
+                        aws_access_key_id=aak, 
+                        aws_secret_access_key=ask)
+   
+    s3_filename = os.path.basename(filename)
     
-    print(f'Uploading {mosaic_filepath} to s3.')
+    print(f'Uploading {s3_filename} to s3.')
 
-    s3.meta.client.upload_file(mosaic_filepath, 
+    s3.meta.client.upload_file(filename, 
                               'restoration-monitoring', 
-                              'plantation-mapping/data/samples/' + suffix)
+                              'plantation-mapping/data/samples/' + s3_filename)
 
     return None
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
    
-    import argparse
-    parser = argparse.ArgumentParser()
-    print("Argument List:", str(sys.argv))
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     print("Argument List:", str(sys.argv))
 
-    parser.add_argument('--country', dest='country', type=str)
-    parser.add_argument('--model', dest='model', type=str)
-    parser.add_argument('--compile', dest='compile_from', type=str)
+#     parser.add_argument('--country', dest='country', type=str)
+#     parser.add_argument('--model', dest='model', type=str)
+#     parser.add_argument('--compile', dest='compile_from', type=str)
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
     
-    mosaic_tif(args.country, args.model, args.compile_from)
+#     mosaic_tif(args.country, args.model, args.compile_from)
